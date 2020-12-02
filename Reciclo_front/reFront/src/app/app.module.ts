8f3e0205e4457c50e38931a9d7072b72a0bedb51
Wesley Silva <wesley.bueno.silva.wbs@gmail.com>
2020-12-02 11:58:15 -0300

    estrutura, home,login,cadastro e shop

---
+++ Reciclo_front/reFront/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    CadastroComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
