460312785b4970319494d2f2421591b885d45eff
Wesley Silva <wesley.bueno.silva.wbs@gmail.com>
2020-12-09 10:45:24 -0300

    navbar com login

--- Reciclo_front/reFront/src/app/footer/footer.component.ts
+++ Reciclo_front/reFront/src/app/footer/footer.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import{faInstagram} from '@fortawesome/free-brands-svg-icons'
 import{faGithub} from '@fortawesome/free-brands-svg-icons'
 import{faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
-=======
->>>>>>> wesley
 
 @Component({
   selector: 'app-footer',
@@ -13,20 +10,13 @@ import{faLinkedin} from '@fortawesome/free-brands-svg-icons'
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-<<<<<<< HEAD
 faInstagram=faInstagram
 faGithub=faGithub
 faLinkedin=faLinkedin
-=======
->>>>>>> wesley
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> wesley
